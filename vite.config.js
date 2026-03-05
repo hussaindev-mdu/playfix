457b27f80cbc68027230f6b4257eef6c69827f7a
hussaindev-mdu <hussaindev07@gmail.com>
2026-03-06 01:43:37 +0530

    gh pages added

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] || 'playfix';
+
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: process.env.GITHUB_PAGES === 'true' ? `/${repoName}/` : '/'
 });
